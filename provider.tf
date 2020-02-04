e7973dd2a77224149b9c9f40c545b2d5528516e7
KalluruS <>
2020-02-04 20:00:39 +0530

    Adding files

--- provider.tf
+++ provider.tf
@@ -2,3 +2,65 @@ provider "azurerm" {
   subscription_id = "522fb493-ffab-4a5c-b72d-fe645a4d1e41"
 }
 
+resource "azurerm_virtual_machine" "main" {
+  name                  = "sample-vm"
+  location              = "East US"
+  resource_group_name   = "OpenShift"
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  delete_os_disk_on_termination = true
+
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
+
+
+resource "azurerm_network_interface" "main" {
+  name                = "sample-nic"
+  location            = "East US"
+  resource_group_name = "OpenShift"
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = "default"
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+
+resource "azurerm_managed_disk" "example" {
+  name                 = "acctestmd1"
+  location             = "East US"
+  resource_group_name  = "OpenShift"
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "1"
+
+  tags = {
+    environment = "disk1"
+  }
+}
